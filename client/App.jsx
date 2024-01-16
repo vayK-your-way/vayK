8d8dcfabda012be16f95b4379b90012d6aaec4f3
Michelle Xie <mxie97@gmail.com>
2024-01-16 09:32:57 -0800

    initial setup

---
+++ client/App.jsx
@@ -0,0 +1,13 @@
+import HomePage from './Components/----';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route
+        path='/'
+        element={<HomePage />}
+      />
+    </Routes>
+  );
+};
+export default App;
