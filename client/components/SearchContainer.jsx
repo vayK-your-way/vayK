a56a17ae10365734e55d169285db37061a3c99fa
Joshua McDaniel <joshuamcdaniel@Joshys-MacBook-Pro.local>
2024-01-17 10:27:38 -0700

    Fixed back end route issues. Search button successfully making fetches.

--- client/components/SearchContainer.jsx
+++ client/components/SearchContainer.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
 
 
 const SearchContainer = () => {
+
+  const [rentalList, setRentalList] = useState([]);
+
+  function handleSearch() {
+    const destinationInput = document.getElementById('destinationInput').value;
+    const checkIn = document.getElementById('checkIn').value;
+    const checkOut = document.getElementById('checkOut').value;
+
+    fetch(`http://localhost:3000/api/airbnb-info?query=${destinationInput}&checkIn=${checkIn}&checkOut=${checkOut}`, {
+      method: 'GET'
+    })
+      .then((res) => console.log(res))
+      .then((res) => setRentalList(res))
+  }
+
   return (
     <div className='font-montserrat border rounded p-2'>
       <div>
@@ -14,13 +30,13 @@ const SearchContainer = () => {
         </div>
         <div>
           <h2>Check-in:</h2>
-          <input className='border-secondary border rounded' placeholder='MM/DD/YY' type='text' id='check-in'></input>
+          <input className='border-secondary border rounded' placeholder='YYYY-MM-DD' type='text' id='checkIn'></input>
           <h2>Check-out:</h2>
-          <input className='border-secondary border rounded' placeholder="MM/DD/YY" type='text' id='check-out'></input>
+          <input className='border-secondary border rounded' placeholder="YYYY-MM-DD" type='text' id='checkOut'></input>
         </div>
         <div>
           <br />
-          <button className='border-secondary border rounded p-1'>Run Search</button>
+          <button onClick={handleSearch} className='border-secondary border rounded p-1'>Run Search</button>
         </div>
       </div>
     </div>
--- src/routes/star.js
+++ src/routes/star.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-import cookieController from '../controllers/cookieController';
 
-router.post(
-  '/add', //cookieController to verify there is a cookie stored in the user's browser, //userController to add listing to user's starred list, (req, res) =>
-  res.status(200).json(),
-);
+router.post('/add', (req, res) => {
+  res.status(200).json()
+});
+//cookieController to verify there is a cookie stored in the user's browser, //userController to add listing to user's starred list, (req, res) =>
+
 
 module.exports = router;
